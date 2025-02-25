eb005190859e133ca303e96ca8e7b195d10cdbf8
J-Rickens <88795835+J-Rickens@users.noreply.github.com>
2025-02-25 15:57:18 -0500

    Update test_iteratorIntegration.py valid test cases

--- test/integrationtest/util/test_iteratorIntegration.py
+++ test/integrationtest/util/test_iteratorIntegration.py
@@ -29,10 +29,16 @@ class TestIteratorIntagration:
     test: Valid calls
     '''
     @pytest.mark.iterate
-    @pytest.mark.parametrize('', [
-    	()
+    @pytest.mark.parametrize('num, digits, data, expectedbool, expecteddata', [
+    	(6174, 4, None, True, [6174,6174]),
+        (9532, 4, [1111,2222,3333,4444,5555,6666,7777,8888,9999,1110,9532], True, [1111,2222,3333,4444,5555,6666,7777,8888,9999,1110,9532,8082,8532,6174,6174]),
+        (2, 3, None, True, [2,198,791,791]),
+        (7, 1, [7], True, [7,0,0]),
+        (31, 0, None, True, [31,18,63,27,45,9,0,0])
     ], ids=[
-    	''
+    	'6174-4', '9541-4', '2-3', '7-1', '31-0'
     ])
-    def testIteratorRearrangerValid(self):
-        pass
+    def testIteratorRearrangerValid(self, num, digits, data, expectedbool, expecteddata):
+        actualbool, actualdata = self.iteratorObj(num, digits, data)
+        assert actualbool == expectedbool
+        assert actualdata == expecteddata
